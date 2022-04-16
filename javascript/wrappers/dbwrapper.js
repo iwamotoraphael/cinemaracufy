e40148fb46ae5089f63143e4179303164261e4d6
iwamotoraphael <iwamotoraphael@gmail.com>
2022-04-16 12:51:20 -0300

    atualização dos códigos de retorno dbwrapper

--- javascript/wrappers/dbwrapper.js
+++ javascript/wrappers/dbwrapper.js
@@ -5,20 +5,19 @@ async function createNetwork(id, nome)
     try{
         let check = await db.query(`SELECT EXISTS(SELECT id_emissora FROM emissora WHERE id_emissora = ${id})`)
         if(check.rows[0].exists)
-            return false
+            return 1
         else
         {
             await db.query(`BEGIN`)
             await db.query(`INSERT INTO emissora VALUES(${id}, '${nome}')`)
             await db.query('COMMIT')
-            return true
+            return 0
         }
     }
     catch(err)
     {
         await db.query('ROLLBACK')
-        console.log(err.stack)
-        return false
+        return 2
     }
 }
 
@@ -27,20 +26,19 @@ async function createCast(id, nome)
     try{
         let check = await db.query(`SELECT EXISTS(SELECT id_pessoa FROM pessoacast WHERE id_pessoa = ${id})`)
         if(check.rows[0].exists)
-            return false
+            return 1
         else
         {
             await db.query(`BEGIN`)
             await db.query(`INSERT INTO pessoacast VALUES(${id}, '${nome}')`)
             await db.query('COMMIT')
-            return true
+            return 0
         }
     }
     catch(err)
     {
         await db.query('ROLLBACK')
-        console.log(err.stack)
-        return false
+        return 2
     }
 }
 
@@ -49,20 +47,19 @@ async function createGenre(id, nome)
     try{
         let check = await db.query(`SELECT EXISTS(SELECT id_genero FROM genero WHERE id_genero = ${id})`)
         if(check.rows[0].exists)
-            return false
+            return 1
         else
         {
             await db.query(`BEGIN`)
             await db.query(`INSERT INTO genero VALUES(${id}, '${nome}')`)
             await db.query('COMMIT')
-            return true
+            return 0
         }
     }
     catch(err)
     {
         await db.query('ROLLBACK')
-        console.log(err.stack)
-        return false
+        return 2
     }
 }
 
@@ -71,20 +68,19 @@ async function createCreator(id, nome)
     try{
         let check = await db.query(`SELECT EXISTS(SELECT id_criador FROM criador WHERE id_criador = ${id})`)
         if(check.rows[0].exists)
-            return false
+            return 1
         else
         {
             await db.query(`BEGIN`)
             await db.query(`INSERT INTO criador VALUES(${id}, '${nome}')`)
             await db.query('COMMIT')
-            return true
+            return 0
         }
     }
     catch(err)
     {
         await db.query('ROLLBACK')
-        console.log(err.stack)
-        return false
+        return 2
     }
 }
 
@@ -93,20 +89,19 @@ async function createProvider(id, nome)
     try{
         let check = await db.query(`SELECT EXISTS(SELECT FROM plataforma WHERE id_plataforma = ${id})`)
         if(check.rows[0].exists)
-            return false
+            return 1
         else
         {
             await db.query(`BEGIN`)
             await db.query(`INSERT INTO plataforma VALUES(${id}, '${nome}')`)
             await db.query('COMMIT')
-            return true
+            return 0
         }
     }
     catch(err)
     {
         await db.query('ROLLBACK')
-        console.log(err.stack)
-        return false
+        return 2
     }
 }
 
@@ -115,20 +110,19 @@ async function createCompany(id, nome)
     try{
         let check = await db.query(`SELECT EXISTS(SELECT FROM companhia WHERE id_companhia = ${id})`)
         if(check.rows[0].exists)
-            return false
+            return 1
         else
         {
             await db.query(`BEGIN`)
             await db.query(`INSERT INTO companhia VALUES(${id}, '${nome}')`)
             await db.query('COMMIT')
-            return true
+            return 0
         }
     }
     catch(err)
     {
         await db.query('ROLLBACK')
-        console.log(err.stack)
-        return false
+        return 2
     }
 }
 
@@ -136,20 +130,20 @@ async function createUser(nome, id_avatar, hash_senha){
     try{
         let check = await db.query(`SELECT EXISTS(SELECT nome_usuario FROM usuario WHERE nome_usuario='${nome}')`)
         if(check.rows[0].exists)
-            return false
+            return 1
         else
         {
             await db.query(`BEGIN`)
             await db.query(`INSERT INTO usuario(nome_usuario, hash_senha, id_avatar) VALUES('${nome}', '${hash_senha}', '${id_avatar}')`)
             await db.query('COMMIT')
-            return true
+            return 0
         }
     }
     catch(err)
     {
         await db.query('ROLLBACK')
         console.log(err.stack)
-        return false
+        return 2
     }
 }
 
@@ -158,35 +152,29 @@ async function createItem(id_item, lancamento, nome_item)
     try{
         let check = await db.query(`SELECT EXISTS(SELECT id_item FROM itemsistema WHERE id_item='${id_item}')`)
         if(check.rows[0].exists)
-            return false
+            return 1
         else
         {
             await db.query(`BEGIN`)
             await db.query(`INSERT INTO itemsistema VALUES(${id_item}, ${lancamento}, '${nome_item}')`)
             await db.query('COMMIT')
-            return true
+            return 0
         }
     }
     catch(err)
     {
         await db.query('ROLLBACK')
-        console.log(err.stack)
-        return false
+        return 2
     }
 }
 
 
-
-async function createMovie(id_item)
+//TO-DO
+/* O json recebido no método deve ser o mesmo retornado pela API */
+async function createMovie(jsaon)
 {
     try{
-        let check = await db.query(`SELECT EXISTS(SELECT id_item FROM filme WHERE id_item='${id_item}')`)
-        if(check.rows[0].exists)
-            return false
-        else
-        {
-          
-        }
+        
     }
     catch(err)
     {
@@ -200,5 +188,11 @@ async function createMovie(id_item)
 module.exports = {
     createUser,
     createItem,
+    createCast,
+    createCompany,
+    createCreator,
+    createGenre,
+    createNetwork,
+    createProvider,
     createMovie
 }
