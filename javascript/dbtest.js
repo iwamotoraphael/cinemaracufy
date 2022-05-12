eaf6bcf432a32869a4d3d4930b51b600caf89fe8
iwamotoraphael <iwamotoraphael@gmail.com>
2022-05-12 10:26:54 -0300

    refatoração dos erros

--- javascript/dbtest.js
+++ javascript/dbtest.js
@@ -6,7 +6,7 @@ async function main(){
         
         let resp = await api.getTVDetails(92749)
         let ret = await dbw.createItem(resp.data.name, resp.data.id, 'https://image.tmdb.org/t/p/original'+resp.data.poster_path, false)
-        console.log(ret)
+        console.log(ret) 
     } 
     catch(err)
     {
--- javascript/wrappers/dbwrapper.js
+++ javascript/wrappers/dbwrapper.js
@@ -3,18 +3,18 @@ const db = require('../config/_dbconfig')
     async function createNetwork(nome, foto)
     {
         try{
-            let check = await db.query(`SELECT EXISTS(SELECT nome_emissora FROM emissora WHERE nome_emissora = $1)`, [nome])
-            if(check.rows[0].exists)
-                return 1
-            else
-            {
-                await db.query(`INSERT INTO emissora(nome_emissora, logo_emissora) VALUES($1, $2)`, [nome, foto])
-                return 0
-            }
+            
+            await db.query(`INSERT INTO emissora(nome_emissora, logo_emissora) VALUES($1, $2)`, [nome, foto])
+            return 0
+            
         }
         catch(err)
         {
-            return 2
+            //error code 23505 means primary key already exists
+            if(err.code == 23505)
+                return 1
+            else
+                return 2
         }
     }
 
@@ -33,18 +33,16 @@ const db = require('../config/_dbconfig')
     async function createGenre(nome)
     {
         try{
-            let check = await db.query(`SELECT EXISTS(SELECT nome_genero FROM genero WHERE nome_genero = $1)`, [nome])
-            if(check.rows[0].exists)
-                return 1
-            else
-            {
-                await db.query(`INSERT INTO genero(nome_genero) VALUES($1)`, [nome])
-                return 0
-            }
+            await db.query(`INSERT INTO genero(nome_genero) VALUES($1)`, [nome])
+            return 0
         }
         catch(err)
         {
-            return 2
+            //error code 23505 means primary key already exists
+            if(err.code == 23505)
+                return 1
+            else
+                return 2
         }
     }
 
@@ -63,146 +61,129 @@ const db = require('../config/_dbconfig')
     async function createProvider(nome, foto)
     {
         try{
-            let check = await db.query(`SELECT EXISTS(SELECT nome_plataforma FROM plataforma WHERE nome_plataforma = $1)`, [nome])
-            if(check.rows[0].exists)
-                return 1
-            else
-            {
-                await db.query(`INSERT INTO plataforma(nome_plataforma, logo_plataforma) VALUES($1, $2)`, [nome, foto])
-                return 0
-            }
+            await db.query(`INSERT INTO plataforma(nome_plataforma, logo_plataforma) VALUES($1, $2)`, [nome, foto])
+            return 0
         }
         catch(err)
         {
-            return 2
+            //error code 23505 means primary key already exists
+            if(err.code == 23505)
+                return 1
+            else
+                return 2
         }
     }
 
     async function createCompany(nome, foto)
     {
         try{
-            let check = await db.query(`SELECT EXISTS(SELECT FROM companhia WHERE nome_companhia = $1)`, [nome])
-            if(check.rows[0].exists)
-                return 1
-            else
-            {
-                await db.query(`INSERT INTO companhia(nome_companhia, logo_companhia) VALUES($1, $2)`, [nome, foto])
-                return 0
-            }
+            await db.query(`INSERT INTO companhia(nome_companhia, logo_companhia) VALUES($1, $2)`, [nome, foto])
+            return 0
         }
         catch(err)
         {
-            return 2
+            //error code 23505 means primary key already exists
+            if(err.code == 23505)
+                return 1
+            else
+                return 2
         }
     }
 
     async function createUser(nome, id_avatar, hash_senha){
         try{
-            let check = await db.query(`SELECT EXISTS(SELECT nome_usuario FROM usuario WHERE nome_usuario=$1)`, [nome])
-            if(check.rows[0].exists)
-                return 1
-            else
-            {
-                await db.query(`INSERT INTO usuario(nome_usuario, hash_senha, id_avatar) VALUES($1, $2, $3)`, [nome, hash_senha, id_avatar])
-                return 0
-            }
+            await db.query(`INSERT INTO usuario(nome_usuario, hash_senha, id_avatar) VALUES($1, $2, $3)`, [nome, hash_senha, id_avatar])
+            return 0
         }
         catch(err)
         {
-            return 2
+            //error code 23505 means primary key already exists
+            if(err.code == 23505)
+                return 1
+            else
+                return 2
         }
     }
 
     async function createItem(nome_item, id_tmdb, poster_item, tipo)
     {
         try{
-            let check = await db.query(`SELECT EXISTS(SELECT id_tmdb FROM itemsistema WHERE id_tmdb=$1)`, [id_tmdb])
-            if(check.rows[0].exists)
-                return 1
+            await db.query(`BEGIN`)
+            let id = await db.query(`INSERT INTO itemsistema(nome_item, id_tmdb, poster_item, tipo) VALUES($1, $2, $3, $4) RETURNING id_item`, [nome_item, id_tmdb, poster_item, tipo])
+
+            id = id.rows[0].id_item
+            
+
+            if(tipo)
+                {
+                    if(await createMovie(id) == 2)
+                        return 2
+                }
             else
-            {
-                await db.query(`BEGIN`)
-                let id = await db.query(`INSERT INTO itemsistema(nome_item, id_tmdb, poster_item, tipo) VALUES($1, $2, $3, $4) RETURNING id_item`, [nome_item, id_tmdb, poster_item, tipo])
-
-                id = id.rows[0].id_item
-                
-
-                if(tipo)
-                    {
-                        if(await createMovie(id) == 2)
-                            return 2
-                    }
-                else
-                    {
-                        if(await createSerie(id) == 2)
-                            return 2
-                    }
-                
-                await db.query('COMMIT')
-                
-                return 0
-            }
+                {
+                    if(await createSerie(id) == 2)
+                        return 2
+                }
+            
+            await db.query('COMMIT')
+            
+            return 0
         }
         catch(err)
         {
             await db.query('ROLLBACK')
-            console.log(err.stack)
-            return 2
+            //error code 23505 means primary key already exists
+            if(err.code == 23505)
+                return 1
+            else
+                return 2
         }
     }
 
     async function createMovie(id)
     {
         try{
-            let check1 = await db.query(`SELECT EXISTS(SELECT id_item FROM filme WHERE id_item=$1)`, [id])
-            if(check1.rows[0].exists)
-                return 1
-            else
-            {
-                await db.query(`INSERT INTO filme(id_item) VALUES($1)`, [id])
-                return 0
-            }
+            await db.query(`INSERT INTO filme(id_item) VALUES($1)`, [id])
+            return 0
         }
         catch(err)
         {
-            console.log(err.stack)
-            return 2
+            //error code 23505 means primary key already exists
+            if(err.code == 23505)
+                return 1
+            else
+                return 2
         }
     }
 
     async function createSerie(id)
     {
         try{
-            let check1 = await db.query(`SELECT EXISTS(SELECT id_item FROM serie WHERE id_item=$1)`, [id])
-            if(check1.rows[0].exists)
-                return 1
-            else
-            {
-                await db.query(`INSERT INTO serie(id_item) VALUES($1)`, [id])
-                return 0
-            }
+            await db.query(`INSERT INTO serie(id_item) VALUES($1)`, [id])
+            return 0
         }
         catch(err)
         {
-            console.log(err.stack)
-            return 2
+            //error code 23505 means primary key already exists
+            if(err.code == 23505)
+                return 1
+            else
+                return 2
         }
     }
 
     async function createSeason(id_item, numero_temporada, titulo){
         try{
-            let check = await db.query(`SELECT EXISTS(SELECT id_item, numero_temporada FROM temporada WHERE id_item = $1 AND numero_temporada = $2)`, [id_item, numero_temporada])
-            if(check.rows[0].exists)
-                return 1
-            else
-            {
-                await db.query(`INSERT INTO temporada VALUES($1,$2,$3)`, [numero_temporada, id_item, titulo])
-                return 0
-            }
+            await db.query(`INSERT INTO temporada VALUES($1,$2,$3)`, [numero_temporada, id_item, titulo])
+            return 0
         }
         catch(err)
         {
-            return 2
+            //error code 23505 means primary key already exists
+            if(err.code == 23505)
+                return 1
+            else
+                return 2
         }
     }
 
