d3a430b1c5eed81f7f3357f730f959bd35f6190f
iwamotoraphael <iwamotoraphael@gmail.com>
2022-06-01 16:35:32 -0300

    Express e session

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express')
-const pool = require('./javascript/config/_dbconfig')
 const db = require('./javascript/wrappers/dbwrapper')
+const session = require('cookie-session')
 
 const app = express()
 const port = process.env.PORT || 5000
 
+//static files
+app.use("/css", express.static(__dirname+"/css"))
+app.use("/handlebars", express.static(__dirname+"/handlebars"))
+app.use(("/javascript"), express.static(__dirname+"/javascript"))
+
+//Sessão
+app.use(session({
+    name: 'session',
+    secret:'itsmorbintime',
+
+    maxAge: 1000*60*60 //1 hora
+    
+}))
+
+//Routes
+app.get("/",function(req, res){
+    res.render(__dirname + "/handlebars/index.handlebars")
+})
+
+
 
 app.listen(port, ()=>{console.info("servidor rodando na porta: "+port)})
-
